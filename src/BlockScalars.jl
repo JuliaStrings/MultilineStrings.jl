d425bdc48b9267b3f37545adf31240b5853a4809
Curtis Vogt <curtis.vogt@gmail.com>
2020-08-27 11:56:14 -0500

    Initial implementation

--- src/BlockScalars.jl
+++ src/BlockScalars.jl
@@ -1,5 +1,45 @@
 module BlockScalars
 
-# Write your package code here.
+export @blk_str
+
+const DEFAULT_BLOCK_SCALAR = "fs"
+
+function block(str::AbstractString, block_scalar::AbstractString=DEFAULT_BLOCK_SCALAR)
+    style_indicators = intersect(block_scalar, "fl")
+    chomp_indicators = intersect(block_scalar, "csk")
+
+    if length(style_indicators) == 0
+        style_indicator = 'f'
+    elseif length(style_indicators) == 1
+        style_indicator = first(style_indicators)
+    else
+        throw(ArgumentError("Only one block style indicators can be provided"))
+    end
+
+    if length(chomp_indicators) == 0
+        chomp_indicator = 's'
+    elseif length(chomp_indicators) == 1
+        chomp_indicator = first(chomp_indicators)
+    else
+        throw(ArgumentError("Only one block chomp indicators can be provided"))
+    end
+
+    if style_indicator == 'f'
+        str = replace(str, r"(?<=\S)\n(?=\S)" => " ")
+        str = replace(str, r"(?<=\S)\n(\n+)(?!$)" => s"\1")
+    end
+
+    if chomp_indicator == 'c'
+        str = replace(str, r"\n+$" => "\n")
+    elseif chomp_indicator == 's'
+        str = replace(str, r"\n+$" => "")
+    end
+
+    return str
+end
+
+macro blk_str(str::AbstractString, suffix::AbstractString=DEFAULT_BLOCK_SCALAR)
+    block(str, suffix)
+end
 
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,59 @@
-using BlockScalars
+using BlockScalars: @blk_str, block
 using Test
+using YAML: YAML
+
+indent(str, n) = join(map(line -> (" " ^ n) * line, split(str, '\n')), '\n')
+
+function yaml_block(str, block_scalar)
+    yaml = "example: $block_scalar\n$(indent(str, 2))"
+    YAML.load(yaml)["example"]
+end
+
+# https://yaml-multiline.info/
+const TEXT = """
+    Several lines of text,
+    with some "quotes" of various 'types'
+    and also two blank lines:
+
+
+    plus another line at the end.
+
+
+    """
+
+const WHITESPACE = "a\nb\n c \nd\n"
+
+# Validate `yaml_block` function
+@assert yaml_block(TEXT, "|+") == TEXT
+@assert yaml_block(TEXT, "|+") == TEXT
 
 @testset "BlockScalars.jl" begin
-    # Write your tests here.
+    @testset "block: $name" for (name, str) in ("TEXT" => TEXT, "WHITESPACE" => WHITESPACE)
+        @testset "literal" begin
+            @test block(str, "lk") == yaml_block(str, "|+")
+            @test block(str, "lc") == yaml_block(str, "|")
+            @test block(str, "ls") == yaml_block(str, "|-")
+        end
+
+        @testset "folding" begin
+            @test block(str, "fk") == yaml_block(str, ">+")
+            @test block(str, "fc") == yaml_block(str, ">")
+            @test block(str, "fs") == yaml_block(str, ">-")
+        end
+
+        @testset "default style" begin
+            @test block(str, "k") == yaml_block(str, ">+")
+            @test block(str, "c") == yaml_block(str, ">")
+            @test block(str, "s") == yaml_block(str, ">-")
+        end
+
+        @testset "default chomp" begin
+            @test block(str, "l") == yaml_block(str, "|-")
+            @test block(str, "f") == yaml_block(str, ">-")
+        end
+
+        @testset "default" begin
+            @test block(str) == yaml_block(str, ">-")
+        end
+    end
 end
